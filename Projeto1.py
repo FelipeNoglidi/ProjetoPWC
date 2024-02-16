72da089d172422b9c50c81af266fb16776dcd3e2
Felipe Nogueira <felipenogueiralidi@gmail.com>
2024-02-16 00:15:23 -0300

    Segundo commit

--- Projeto1.py
+++ Projeto1.py
@@ -1,15 +1,19 @@
-#Para a resolução de casos simples, esta solução é suficiente. 
+#Agora, para resolver casos mais complexos, fiz uso do RegExr, visto que é mais agradável para lidar com Strings.
+#Neste caso, o que fiz foi encontrar um "espaço" seguido por algum digito númerico "\s(?=\d)" e realizar um split nesse "espaço" em questão.
 
-#Programa de fato
-entrada = input("Informe o endereço desejado: ")
+import re
 
-endereco = entrada.split(" ")
+# Como funcionaria o programa na prática
+ruas = input("Informe o endereço desejado: ")
 
-print(endereco)
+resultado = re.split(r"\s(?=\d)", ruas) #Realiza um split em um local onde há um "espaço seguido por um dígito numérico."
+print(resultado)
 
-#testes com os endereços fornecidos pelo exercício
-entrada = ["Miritiba 339", "Babaçu 500", "Cambuí 123B"]
+#testes com os endereços do exercício
+Array_ruas = ["Rio Branco 23", "Quirino dos Santos 23 b"]
 
-for endereco in entrada:
-  saida = endereco.split(" ")
-  print("\n" + saida[0] + ", " + saida[1])
+for entrada in Array_ruas:
+  saida = re.split(r"\s(?=\d)", entrada)
+
+  print(saida[0])
+  print(saida[1])
